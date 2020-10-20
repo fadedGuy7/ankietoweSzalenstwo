0826a6db24221f3d8c33268625b88bf52200b096
fadedguy7 <kamkraw91@gmail.com>
2020-10-20 20:16:23 +0200

    Updates

---
+++ precache-manifest.84f361e148c90b933d54d10c20609cb1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "949a9d2610a1954eeb29c4a1a60eb7ea",
+    "url": "/ankietoweSzalenstwo/index.html"
+  },
+  {
+    "revision": "25fcfe8c26fd2a9a8bb4",
+    "url": "/ankietoweSzalenstwo/static/css/main.26973927.chunk.css"
+  },
+  {
+    "revision": "fe4f13429b804b298d23",
+    "url": "/ankietoweSzalenstwo/static/js/2.55fc4a68.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/ankietoweSzalenstwo/static/js/2.55fc4a68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25fcfe8c26fd2a9a8bb4",
+    "url": "/ankietoweSzalenstwo/static/js/main.255eac44.chunk.js"
+  },
+  {
+    "revision": "0a59db0a84c7021584a0",
+    "url": "/ankietoweSzalenstwo/static/js/runtime-main.325518fc.js"
+  },
+  {
+    "revision": "1bfdc48454bcf2d76ea04cad669289d4",
+    "url": "/ankietoweSzalenstwo/static/media/c101.1bfdc484.png"
+  },
+  {
+    "revision": "ea6df631d07a6cc6ca6f4439d5da304a",
+    "url": "/ankietoweSzalenstwo/static/media/c102.ea6df631.png"
+  }
+]);
